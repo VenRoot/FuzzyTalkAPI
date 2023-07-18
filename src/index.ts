2df4cd880155beced12080604dee8fb16105ae5c
Ven <0ventox0@gmail.com>
2023-07-19 01:13:59 +0200

    Added websocket

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
 import express from "express";
 import fs from "fs";
+import websocket from "ws";
+
 const app = express();
+
+const wss = new websocket.Server({ port: 3006 });
+
+let interval: NodeJS.Timeout;
+
+wss.on("connection", socket => {
+    socket.on("message", message => {
+        socket.send("Roger that! " + message);
+
+        interval = setInterval(() => {
+            if(fs.existsSync("./src/stest"))
+            {
+                socket.send(`Hello, client! It is currently ${new Date().toLocaleTimeString()}`);
+            }
+        }, 1000)
+    });
+});
+
+wss.on("close", () => {
+    clearInterval(interval);
+    console.log("Client disconnected");
+})
+
+wss.on("listening", () => {
+    console.log("Listening on port 3006");
+});
 
 app.get("/randomImage", (req, res) => {
     const randomImage = Math.floor(Math.random() * 103);
@@ -17,4 +45,7 @@ app.get("/randomImage/:id", (req, res) => {
 });
 
 
-app.listen(3005);
+app.listen(3005, () =>  {
+    console.log("Listening on port 3005");
+});
+
